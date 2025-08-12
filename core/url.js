7d810b1ce42b9513aa1704727eb1a4005c0c03d0
narasimhavaddala <vaddalanarasimha@gmail.com>
2025-08-12 11:38:33 +0530

    init

---
+++ core/url.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const url = "http://localhost:5000";
+
+export const API = axios.create({
+  baseURL: url,
+});
+
+// Add a request interceptor to include the token from localStorage
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token"); // Adjust the key if you're using something like 'accessToken' or JWT
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export const buildImageUrl = (image) => {
+  return image ? `${url}/${image}` : null;
+};
