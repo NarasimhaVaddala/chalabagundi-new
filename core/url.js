bbfadcc4481e49a561302bf0234cff76a9954f83
narasimhavaddala <vaddalanarasimha@gmail.com>
2025-08-22 13:13:11 +0530

    smal change

--- core/url.js
+++ core/url.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
-// const url = "http://localhost:5000";
-const url = "https://backend.chaalabagundhi.com";
+let url;
+
+if (process.env.NODE_ENV === "development") {
+  url = "http://localhost:5000";
+} else {
+  url = "https://backend.chaalabagundhi.com";
+}
 
 export const API = axios.create({
   baseURL: url,
